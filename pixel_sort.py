0f82239a03f730cb725eba10c0230f4f2704b911
ndewy <niklas.dewally@gmail.com>
2021-03-06 22:41:47 +0000

    Implement rudimentary face seperation.
    New functions added to:
    1. Convert between PIL and OpenCV images.
    
    2. Seperate the foreground and background of an image using Otsu's
     algorithm and thresholding  and return this as a binary image
     (cv_threshold).
    
    3. Use the generated binary image to seperate the largest foreground
    region (by area) from a photo, returning an image with just this
    foreground area and a transparent background
    (get_largest_foreground_region).
    
    I intend to use get_largest_foreground_region to selectively apply pixel
    sorts only to the face or starting from the edge of the face.
    
    These functions will only really work well for faces, or other pictures
    which have a predominant subject taking up most of the foreground.

--- pixel_sort.py
+++ pixel_sort.py
@@ -1,9 +1,73 @@
+import cv2
 import numpy as np
 from PIL import Image, ImageChops
 
 BLEND_MODES = {"blend": (lambda img1, img2: Image.blend(img1, img2, 0.6)), "darker": ImageChops.darker,
                "lighter": ImageChops.lighter, "none": (lambda img1, img2: img2), "multiply": ImageChops.multiply, "overlay": ImageChops.overlay, "hardlight": ImageChops.hard_light}
 SORT_FIELDS = {"hue": 0, "saturation": 1, "value": 2}
+
+
+def __pil_to_cv(img):
+    numpy_img = np.array(img.convert("RGB"))
+    opencv_img = cv2.cvtColor(numpy_img, cv2.COLOR_RGB2BGR)
+    return opencv_img
+
+
+def __cv_to_pil(img):
+    color_converted = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    pil_img = Image.fromarray(color_converted)
+    return pil_img
+
+
+def cv_threshold(img):
+    """ Seperates an image into foreground and background using thresholding and Otsu's algorithm.'
+
+    Arguments:
+        img: a BGR OpenCV image.
+
+    Returns: 
+        threshold: an OpenCV Binary image.
+    """
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # gaussian blur image to remove noise
+    blur = cv2.GaussianBlur(gray, (5, 5), 0)
+    # determine optimal threshold value using Otsu, and threshold using it
+    ret, threshold = cv2.threshold(
+        blur, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    return threshold
+
+
+def get_largest_foreground_region(img):
+    """ Creates an image containing only the largest forground region.
+
+        Returns an image with the size of the input. 
+        This image will contain the largest foreground contour (by area) and a transparent background.
+
+        This function works best on portraits or other images with a subject that takes up most of the frame.
+
+        Arguments:
+            img: a PIL image.
+
+        Returns: 
+            img: an RGBA PIL image.
+    """
+
+    opencv_img = __pil_to_cv(img)
+    threshold = cv_threshold(opencv_img)
+    contours, hierarchy = cv2.findContours(
+        threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    contours_byarea = sorted(contours, key=lambda n: cv2.contourArea(n))
+    largest_contour = contours_byarea[-1]
+    # Generate image to draw contour upon
+    new_img = np.zeros((img.height, img.width, 3), np.uint8)
+    # Draw contour onto image
+    new_img = cv2.drawContours(
+        new_img, [largest_contour], 0, (255, 255, 255), thickness=cv2.FILLED)
+    mask = __cv_to_pil(new_img).convert("L")
+    img = img.convert("RGBA")
+    img.putalpha(mask)
+    return img
 
 
 def channel_shift(src_img, change_percent=-0.1, mode="none", channel=0):
@@ -40,6 +104,9 @@ def pixel_sort(src_img, mode="none", sort_field="hue", skip_interval=0):
 
 
 source = Image.open("testimage2.jpg")
+
+__cv_to_pil(cv_threshold(__pil_to_cv(source))).show()
+get_largest_foreground_region(source).show()
 
 image = channel_shift(source)
 image.show()
