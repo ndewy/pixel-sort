87e94780c4ca60cad6847f487f7c0173641d2393
ndewy <niklas.dewally@gmail.com>
2021-03-08 15:53:15 +0000

    More examples

--- examples.py
+++ examples.py
@@ -4,6 +4,8 @@ portrait1 = Image.open("examples/source-images/portrait1.jpg")
 portrait2 = Image.open("examples/source-images/portrait2.jpg")
 moon = Image.open("examples/source-images/moon1.jpg")
 moonnoisy = Image.open("examples/source-images/moon1noisy.jpg")
+cloud = Image.open("examples/source-images/cloud1.jpg")
+galaxy = Image.open("examples/source-images/galaxy1.jpg")
 
 # moon1
 img = moon.transpose(Image.ROTATE_90)
@@ -16,6 +18,11 @@ img = moonnoisy.transpose(Image.ROTATE_90)
 img = pixel_sort.pixel_sort(img,sort_field="saturation",flipped=True,skip_percent=0,mode="none").transpose(Image.ROTATE_270)
 img.save("examples/moon1noisy.jpg","JPEG")
 
+# cloud1 - identical to moon1noisy
+img = pixel_sort.add_noise(cloud,0.003).transpose(Image.ROTATE_90)
+img = pixel_sort.pixel_sort(img,sort_field="saturation",flipped=True,skip_percent=0,mode="none").transpose(Image.ROTATE_270)
+img.save("examples/cloud1.jpg","JPEG")
+
 # portrait1
 portrait1 = Image.open("examples/source-images/portrait1.jpg")
 portrait1.thumbnail((1000,1000))
@@ -24,3 +31,9 @@ mask = pixel_sort.get_largest_foreground_region(noisy,threshold_img=portrait1).t
 img = pixel_sort.pixel_sort(mask,region_max=0.5,region_min=0.2,sort_field="value",skip_percent=0)
 img = img.transpose(Image.ROTATE_270)
 img.save("examples/portrait1.jpg")
+
+# galaxy
+img = pixel_sort.add_noise(galaxy,0.004).transpose(Image.ROTATE_90)
+img = pixel_sort.pixel_sort(img,sort_field="hue",flipped=True,mode="none",region_max=0.1,region_min=0.01,skip_percent=0.01).transpose(Image.ROTATE_270)
+img.show()
+img.save("examples/galaxy.jpg")
--- examples/source-images/NOTICE.md
+++ examples/source-images/NOTICE.md
@@ -5,3 +5,7 @@
 - *portrait2.jpg :* Photo by [Imansyah Muhamad Putera](https://unsplash.com/@imansyahmp?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText) 
 
 - *moon1.jpg*:  Photo by [CHUTTERSNAP](https://unsplash.com/@chuttersnap?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText)
+
+- *cloud1.jpg*: Photo by [C Dustin](https://unsplash.com/@dianamia?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText)
+
+- *galaxy1.jpg:* Photo by [Tengyart](https://unsplash.com/@tengyart?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText)
